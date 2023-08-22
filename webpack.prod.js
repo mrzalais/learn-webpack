ef5fb7921e83f0f84676ebf1a2d256b1914c8570
mrzalais <lejnieksedijs@gmail.com>
2023-08-22 15:49:02 +0300

    Extract CSS and minify HTML/CSS/JS

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,9 @@ const path = require("path");
 const common = require("./webpack.common")
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(
   common,
@@ -10,8 +13,42 @@ module.exports = merge(
     output: {
       filename: "[name].[contenthash].bundle.js",
       path: path.resolve(__dirname, "dist"),
-      assetModuleFilename: "images/[name].[hash][ext]"
+      assetModuleFilename: "images/[name].[contenthash][ext]"
     },
-    plugins: [new CleanWebpackPlugin()]
+    optimization: {
+      minimizer: [
+        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`)
+        `...`,
+        new CssMinimizerPlugin(),
+        new HtmlWebpackPlugin({
+          template: "./src/template.html",
+          minify: {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true
+          }
+        })
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin(
+        {
+          filename: "[name].[contenthash].css"
+        }
+      ),
+      new CleanWebpackPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader, //3. Extract CSS into files
+            "css-loader", //2. Turns css into commonjs
+            "sass-loader"  //1. Turns sass into css
+          ]
+        },
+      ]
+    }
   }
 );
